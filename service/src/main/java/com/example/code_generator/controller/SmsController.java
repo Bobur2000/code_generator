2fc0d94811076efdf4323da75412c55938af2970
HP <boburjon_o@ssd.uz>
2024-08-27 12:25:40 +0500

    first commit project created Spring boot repo collected packages created, dependencies added

---
+++ service/src/main/java/com/example/code_generator/controller/SmsController.java
@@ -0,0 +1,9 @@
+package com.example.code_generator.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sms")
+public class SmsController {
+}
---
+++ service/src/main/java/com/example/code_generator/service/SmsService.java
@@ -0,0 +1,12 @@
+package com.example.code_generator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsService {
+
+
+
+
+
+}
